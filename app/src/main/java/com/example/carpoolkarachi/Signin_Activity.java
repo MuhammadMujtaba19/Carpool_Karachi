8d5d8887c07507801056117884caadcab18ad36a
Mujtaba Bawani <k163863@nu.edu.pk>
2019-07-28 09:46:32 -0700

    updated

--- app/src/main/java/com/example/carpoolkarachi/Signin_Activity.java
+++ app/src/main/java/com/example/carpoolkarachi/Signin_Activity.java
@@ -3,7 +3,7 @@ package com.example.carpoolkarachi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Signin_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/carpoolkarachi/Signup_Activity.java
+++ app/src/main/java/com/example/carpoolkarachi/Signup_Activity.java
@@ -3,11 +3,11 @@ package com.example.carpoolkarachi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Signup_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
+        setContentView(R.layout.activity_signup);
     }
 }
